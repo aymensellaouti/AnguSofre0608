f6628eb750d6b2fff73ace7badb6aef71d58ed01
aymen <aymen.sellaouti@gmail.com>
2019-08-07 08:59:05 +0100

    component

--- src/app/cv/liste/liste.component.ts
+++ src/app/cv/liste/liste.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Personne} from '../../model/personne';
 
 @Component({
@@ -9,6 +9,7 @@ import {Personne} from '../../model/personne';
 export class ListeComponent implements OnInit {
 
   personnes: Personne[];
+  @Output() selectPersonne = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +17,12 @@ export class ListeComponent implements OnInit {
       new Personne(1, 'sellaouti', 'aymen', 'Teacher', 'as.jpg', 37, 7075595 ),
       new Personne(2, 'personne 2', 'aymen', 'Teacher', '404.png', 37, 7075595 ),
     ];
+  }
+
+  sendSelectedPersonne(personne: Personne) {
+    this.selectPersonne.emit(
+      personne
+    );
   }
 
 }
